e4db3848fbb31f00b48b99a841cece018b517f07
ashvini8 <ashvinihunagund@gmail.com>
2022-12-05 19:06:25 -0500

    add's edge case commit

--- app/src/main/java/com/example/schedulab/AdminAdd.java
+++ app/src/main/java/com/example/schedulab/AdminAdd.java
@@ -14,9 +14,13 @@ import android.widget.Toast;
 import com.example.schedulab.databinding.ActivityAdminAddBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -33,12 +37,12 @@ public class AdminAdd extends AdminDrawerBase {
     private CheckBox mFallCheck, mWinterCheck, mSummerCheck;
 
     private EditText editTextName, editTextCode, editTextPreReq;
-            //editTextSessions, editTextPreReq;
+    //editTextSessions, editTextPreReq;
 
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
     private DatabaseReference root = db.getReference().child("Courses");
-
-    private Button back_to_all;
+    private List<String> allCoursesData = new ArrayList<String>();
+    private boolean exists = true;
 
 
     ActivityAdminAddBinding binding;
@@ -49,14 +53,7 @@ public class AdminAdd extends AdminDrawerBase {
         allocateActivityTitle("Add Course");
         setContentView(binding.getRoot());
 
-        back_to_all = findViewById(R.id.back);
-        back_to_all.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent_add = new Intent(AdminAdd.this, AllCourses.class);
-                startActivity(intent_add);
-            }
-        });
+
 
         editTextName = findViewById(R.id.course_name);
         editTextCode = findViewById(R.id.course_code);
@@ -78,14 +75,8 @@ public class AdminAdd extends AdminDrawerBase {
         Boolean checkWinter = mWinterCheck.isChecked();
         Boolean checkSummer = mSummerCheck.isChecked();
 
-        /* check if the course name or course code already exists */
-
-        
-
-        //should change to something more dynamic later
         String[] PQarray = PQinput.split("\\s*,\\s*"); //removes empty spaces at beginning and end of each single string
         List<String> cPreReqs = Arrays.asList(PQarray);
-
 
         Map<String, Boolean> sessions = new HashMap<>();
         sessions.put("aFall", checkFall);
@@ -98,28 +89,86 @@ public class AdminAdd extends AdminDrawerBase {
         entry.setPrereqs(cPreReqs);
         entry.setSessions(sessions);
 
+        /* check if the course name or course code already exists */
+        //to return after
 
-    //following is to push AND pop-up for user to say course has been added
-        root.push().setValue(entry)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
+        //doing a query
+        root.orderByChild("code").equalTo(code).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){ //checks if course code exists
+                    Toast.makeText(AdminAdd.this, "Course Code already exists. Failed to add.", Toast.LENGTH_SHORT).show();
+                }else if(code.equals("")){
+                    Toast.makeText(AdminAdd.this, "Course Code has not been entered. Failed to add.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //actual code goes in here
+                    //also going to check if all the pqs exist in the array list to be created
 
-                        editTextName.setText("");
-                        editTextCode.setText("");
-                        editTextPreReq.setText("");
+                    root.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for (DataSnapshot courses : snapshot.getChildren()) {
+                                Map<String, Object> coursesData = (HashMap<String, Object>) courses.getValue();
+                                String name = (String) coursesData.get("code");
+                                allCoursesData.add(name);
+                            }
+                            exists = true;
+                            for (int ind = 0; ind < cPreReqs.size(); ind++) {
+                                String cPQ = cPreReqs.get(ind);
+                                if (allCoursesData.contains(cPQ) == false) {
+                                    if(cPQ.equals("none") == false){
+                                        exists = false;
+                                        break;
+                                    }
+                                }
+                            }
 
-                        mFallCheck.setChecked(false);
-                        mWinterCheck.setChecked(false);
-                        mSummerCheck.setChecked(false);
+                            if(exists == false){//if it does not exist
+                                Toast.makeText(AdminAdd.this, "One or more prerequistites does not exist. Failed to add.", Toast.LENGTH_SHORT).show();
+                            }else{
+                                //actual final work goes in here
+                                root.push().setValue(entry)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
 
-                        Toast.makeText(AdminAdd.this, "Course Added", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AdminAdd.this, "Error", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                                                editTextName.setText("");
+                                                editTextCode.setText("");
+                                                editTextPreReq.setText("");
+
+                                                mFallCheck.setChecked(false);
+                                                mWinterCheck.setChecked(false);
+                                                mSummerCheck.setChecked(false);
+
+                                                Toast.makeText(AdminAdd.this, "Course Added", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(AdminAdd.this, "Error", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+
+                        }//onDataChange
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });//end of root.addValue....
+
+                }//end inner else
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //the rest should be under an else?
+
     }
 }
